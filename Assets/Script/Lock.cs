9e2754e5e6d1f071fb61e8db1fbab080c154afb3
junmo0524 <123717898+junmo0524@users.noreply.github.com>
2023-05-13 16:34:30 +0900

    .

--- Assets/Script/Lock.cs
+++ Assets/Script/Lock.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
@@ -8,6 +9,7 @@ using UnityEngine.SocialPlatforms.Impl;
 public class Lock : MonoBehaviour
 {
     List<int> datas = new List<int>();
+    List<int> keys = new List<int>() {1,2,3};
 
     void Update()
     {
@@ -18,5 +20,16 @@ public class Lock : MonoBehaviour
             datas.Add(currentScore);
             Debug.Log("Score added: " + currentScore);
         }
+        bool result = Enumerable.SequenceEqual(datas, keys);
+        Debug.Log(result);
+        //PrintList();
     }
+    /*void PrintList()
+    {
+        Debug.Log("리스트에 저장된 값:");
+        foreach (int value in datas)
+        {
+            Debug.Log(value);
+        }
+    }*/
 }
